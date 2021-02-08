2942cdbd0084a844642e81612449ccbaf54e0a5f
kocacigim <55958321+kocacigim@users.noreply.github.com>
2021-02-08 18:00:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ client.on("message", (channel, tags, message,) => {
     return
     }
 
-    if (message.startsWith("!bttv @")) {
+    if (message.startsWith("!bttv")) {
       var input = message.split(' ')[1];
-         clientSay('videoyun', "@"+input + ` Hangi tarayıcıyı kullandığının önemi yok, eğer bilgisayardaysan https://betterttv.com/ bu siteye gir ve eklentiyi kur ve mükemmel über süper güzel emoteleri sen de gör. Denemek istemez misin? Hemen tıkla, hemen insin, hemen mutlu ol.` ); 1500
+         clientSay('videoyun', "@" + tags["username"] + ` Hangi tarayıcıyı kullandığının önemi yok, eğer bilgisayardaysan https://betterttv.com/ bu siteye gir ve eklentiyi kur ve mükemmel über süper güzel emoteleri sen de gör. Denemek istemez misin? Hemen tıkla, hemen insin, hemen mutlu ol.` ); 1500
     return
     }
 	
